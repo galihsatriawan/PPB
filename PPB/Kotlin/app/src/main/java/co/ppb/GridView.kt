0d9e248588df294ce089870cc3a7f03fc55cf447
galihsatriawan <satriawan.galih99@gmail.com>
2018-10-01 17:59:34 +0700

    Penambahan Clickable pada GridView dan ListView

--- PPB/Kotlin/app/src/main/java/co/ppb/GridView.kt
+++ PPB/Kotlin/app/src/main/java/co/ppb/GridView.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import co.ppb.Adapter.MahasiswaAdapter
 import co.ppb.Model.Mahasiswa
 import kotlinx.android.synthetic.main.activity_grid_view.*
@@ -18,18 +20,26 @@ class GridView : AppCompatActivity() {
         setContentView(R.layout.activity_grid_view)
 
         // load mahasiswa
-        mahasiswaList.add(Mahasiswa("Coffee","1", R.drawable.abc_ic_go_search_api_material))
-        mahasiswaList.add(Mahasiswa("Coffee","2", R.drawable.abc_ic_go_search_api_material))
-        mahasiswaList.add(Mahasiswa("Coffee","3", R.drawable.abc_ic_go_search_api_material))
-        mahasiswaList.add(Mahasiswa("Coffee","4", R.drawable.abc_ic_go_search_api_material))
+        mahasiswaList.add(Mahasiswa("Galih","161111077", R.drawable.foto1))
+        mahasiswaList.add(Mahasiswa("Desy","161111078", R.drawable.foto2))
+        mahasiswaList.add(Mahasiswa("Nadya","161111079", R.drawable.foto2))
+        mahasiswaList.add(Mahasiswa("Bibie","161111080", R.drawable.foto1))
+        mahasiswaList.add(Mahasiswa("Luke","161111081", R.drawable.foto3))
 
         adapter = MahasiswaAdapter(this, mahasiswaList,1)
 
         gvMhs.adapter = adapter
+        gvMhs.onItemClickListener = object : AdapterView.OnItemClickListener{
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = mahasiswaList?.get(position)
+                Toast.makeText(this@GridView,"Nama : "+selectedItem.nama+" NRP : "+selectedItem.nrp,Toast.LENGTH_SHORT).show()
+            }
+
+        }
         btnListView.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@GridView,ListView::class.java)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
             finish()
         })
--- PPB/Kotlin/app/src/main/java/co/ppb/ListView.kt
+++ PPB/Kotlin/app/src/main/java/co/ppb/ListView.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import co.ppb.Adapter.MahasiswaAdapter
 import co.ppb.Model.Mahasiswa
 import kotlinx.android.synthetic.main.activity_list_view.*
@@ -16,16 +18,26 @@ class ListView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
-        mhsList!!.add(Mahasiswa(
-                "Galih","1",R.drawable.abc_ic_go_search_api_material
-        ))
+
+        mhsList!!.add(Mahasiswa("Galih","161111077", R.drawable.foto1))
+        mhsList!!.add(Mahasiswa("Desy","161111078", R.drawable.foto2))
+        mhsList!!.add(Mahasiswa("Nadya","161111079", R.drawable.foto2))
+        mhsList!!.add(Mahasiswa("Bibie","161111080", R.drawable.foto1))
+        mhsList!!.add(Mahasiswa("Luke","161111081", R.drawable.foto3))
+
         adapter = MahasiswaAdapter(this,mhsList!!,2)
         lvMahasiswa.adapter = adapter
+        lvMahasiswa.onItemClickListener = object:AdapterView.OnItemClickListener{
+            override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var selected = mhsList?.get(position)
+                Toast.makeText(this@ListView,"NRP : "+selected.nrp+" Nama : "+selected.nama,Toast.LENGTH_SHORT).show()
+            }
 
+        }
         btnGrid.setOnClickListener(View.OnClickListener {
             val intent = Intent(this@ListView, GridView::class.java)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
             finish()
         })
